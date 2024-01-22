8695793455460daae514a92808d83eb40fa535b9
Phuong Tran <ptran@my365.bellevue.edu>
2024-01-21 19:22:18 -0800

    Add files via upload

---
+++ week-2/tran_myworld.py
@@ -0,0 +1,9 @@
+
+"""
+    Title: tran_myworld.py
+    Author: Phuong Tran
+    Date: 01/21/2024
+    Description: First python app creation.
+"""
+name = 'Tran'
+print("You are now in {}'s world!".format(name))
